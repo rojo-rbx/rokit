ba74928e9da2bfa8f417e1aaf55aa2108dcfdc8b
Filip Tibell <filip.tibell@gmail.com>
2024-03-21 23:12:39 +0100

    Make src-new the src dir, move src to src-old

---
+++ src-old/cli/mod.rs
@@ -0,0 +1,224 @@
+use std::collections::BTreeMap;
+use std::env::current_dir;
+use std::path::PathBuf;
+
+use anyhow::{bail, Context};
+use clap::Parser;
+
+use crate::home::Home;
+use crate::manifest::Manifest;
+use crate::tool_alias::ToolAlias;
+use crate::tool_name::ToolName;
+use crate::tool_spec::ToolSpec;
+use crate::tool_storage::{InstalledToolsCache, ToolStorage};
+use crate::trust::{TrustCache, TrustMode};
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+pub struct Args {
+    #[clap(subcommand)]
+    pub subcommand: Subcommand,
+}
+
+impl Args {
+    pub async fn run(self, home: &Home, tools: ToolStorage) -> anyhow::Result<()> {
+        match self.subcommand {
+            Subcommand::Init(sub) => sub.run(),
+            Subcommand::List(sub) => sub.run(home).await,
+            Subcommand::Add(sub) => sub.run(tools).await,
+            Subcommand::Install(sub) => sub.run(tools).await,
+            Subcommand::Trust(sub) => sub.run(home).await,
+            Subcommand::SelfInstall(sub) => sub.run(home, tools).await,
+
+            Subcommand::Update(_) => bail!("This command is not yet implemented."),
+            Subcommand::SelfUpdate(_) => bail!("This command is not yet implemented."),
+        }
+    }
+}
+
+#[derive(Debug, Parser)]
+pub enum Subcommand {
+    Init(InitSubcommand),
+    List(ListSubcommand),
+    Add(AddSubcommand),
+    Update(UpdateSubcommand),
+    Install(InstallSubcommand),
+    Trust(TrustSubcommand),
+    SelfUpdate(SelfUpdateSubcommand),
+    SelfInstall(SelfInstallSubcommand),
+}
+
+/// Initialize a new Aftman manifest file.
+#[derive(Debug, Parser)]
+pub struct InitSubcommand {
+    /// The folder to initialize the manifest file in. Defaults to the current
+    /// directory.
+    pub path: Option<PathBuf>,
+}
+
+impl InitSubcommand {
+    pub fn run(self) -> anyhow::Result<()> {
+        let path = match self.path {
+            Some(v) => v,
+            None => current_dir().context("Could not read current directory")?,
+        };
+
+        Manifest::init_local(&path)?;
+
+        Ok(())
+    }
+}
+
+/// Lists all existing tools managed by Aftman.
+#[derive(Debug, Parser)]
+pub struct ListSubcommand {}
+
+impl ListSubcommand {
+    pub async fn run(self, home: &Home) -> anyhow::Result<()> {
+        let installed_path = home.path().join("tool-storage").join("installed.txt");
+        let installed = InstalledToolsCache::read(&installed_path).await?;
+
+        let mut tools = BTreeMap::new();
+
+        for tool in installed.tools {
+            tools
+                .entry(tool.name().to_string())
+                .or_insert(Vec::new())
+                .push(tool.version().clone());
+        }
+
+        for (tool, mut versions) in tools {
+            println!("{tool}");
+
+            versions.sort();
+            versions.reverse();
+
+            let versions = versions
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<_>>()
+                .join(", ");
+
+            println!("  {versions}");
+        }
+
+        Ok(())
+    }
+}
+
+/// Adds a new tool to Aftman and installs it.
+#[derive(Debug, Parser)]
+pub struct AddSubcommand {
+    /// A tool spec describing where to get the tool and what version to
+    /// install.
+    pub tool_spec: ToolSpec,
+
+    /// The name that will be used to run the tool.
+    pub tool_alias: Option<ToolAlias>,
+
+    /// Install this tool globally by adding it to ~/.aftman/aftman.toml instead
+    /// of installing it to the nearest aftman.toml file.
+    #[clap(long)]
+    pub global: bool,
+}
+
+impl AddSubcommand {
+    pub async fn run(self, tools: ToolStorage) -> anyhow::Result<()> {
+        tools
+            .add(&self.tool_spec, self.tool_alias.as_ref(), self.global)
+            .await
+    }
+}
+
+/// Updates one or more tools that are managed by Aftman.
+///
+/// Tools can be specified either by their alias or by their name.
+///
+/// If no tools are listed, Aftman will update all installed tools.
+#[derive(Debug, Parser)]
+pub struct UpdateSubcommand {
+    /// One or more tools to update. If no tools are given, update all tools.
+    pub aliases_or_specs: Vec<String>,
+
+    /// Update this tool globally by adding it to ~/.aftman/aftman.toml instead
+    /// of installing it to the nearest aftman.toml file that mentions it.
+    #[clap(long)]
+    pub global: bool,
+
+    /// Ignore semantic versioning and upgrade to the latest stable versions.
+    #[clap(long)]
+    pub latest: bool,
+}
+
+/// Install all tools listed by Aftman files from the current directory.
+#[derive(Debug, Parser)]
+pub struct InstallSubcommand {
+    /// Skip checking if these tools have been installed before. It is
+    /// recommended to only run this on CI machines.
+    #[clap(long)]
+    pub no_trust_check: bool,
+    /// Skip / don't error if a tool was not trusted during install.
+    #[clap(long)]
+    pub skip_untrusted: bool,
+    /// Force install tools, even if they are already installed.
+    #[clap(long)]
+    pub force: bool,
+}
+
+impl InstallSubcommand {
+    pub async fn run(self, tools: ToolStorage) -> anyhow::Result<()> {
+        let trust = if self.no_trust_check {
+            TrustMode::NoCheck
+        } else {
+            TrustMode::Check
+        };
+
+        tools
+            .install_all(trust, self.force, self.skip_untrusted)
+            .await
+    }
+}
+
+/// Mark the given tool name as being trusted.
+#[derive(Debug, Parser)]
+pub struct TrustSubcommand {
+    /// The tool to mark as trusted.
+    pub name: ToolName,
+}
+
+impl TrustSubcommand {
+    pub async fn run(self, home: &Home) -> anyhow::Result<()> {
+        if TrustCache::add(home, self.name.clone()).await? {
+            tracing::info!("Added {} to the set of trusted tools.", self.name);
+        } else {
+            tracing::info!("{} was already a trusted tool.", self.name);
+        }
+
+        Ok(())
+    }
+}
+
+/// Update Aftman from the internet.
+#[derive(Debug, Parser)]
+pub struct SelfUpdateSubcommand {}
+
+/// Install Aftman and update all references to it. Run this command if you've
+/// just upgraded Aftman manually.
+#[derive(Debug, Parser)]
+pub struct SelfInstallSubcommand {}
+
+impl SelfInstallSubcommand {
+    pub async fn run(self, home: &Home, tools: ToolStorage) -> anyhow::Result<()> {
+        tools.update_links().await?;
+
+        if crate::system_path::add(home)? {
+            tracing::info!(
+                "Added ~/.aftman/bin to your PATH. Restart your terminal for this to take effect."
+            );
+        } else {
+            tracing::debug!("Did not modify PATH.");
+        }
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,28 @@
-mod auth;
-mod cli;
-mod config;
-mod dirs;
-mod home;
-mod ident;
-mod manifest;
-mod process;
-mod system_path;
-mod tool_alias;
-mod tool_id;
-mod tool_name;
-mod tool_source;
-mod tool_spec;
-mod tool_storage;
-mod trust;
-
-use std::env::{consts::EXE_SUFFIX, current_dir, current_exe};
-
-use anyhow::{bail, format_err, Context};
+use anyhow::Result;
 use clap::Parser;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::EnvFilter;
 
-use crate::auth::AuthManifest;
-use crate::cli::Args;
-use crate::home::Home;
-use crate::manifest::Manifest;
-use crate::tool_storage::ToolStorage;
+mod cli;
+use cli::Args;
 
-async fn run() -> anyhow::Result<()> {
-    let home = Home::from_env()?;
-    let tool_storage = ToolStorage::new(&home).await?;
-    let exe_name = current_exe_name()?;
-
-    if exe_name != "aftman" {
-        let start_dir = current_dir().context("Failed to find current working directory")?;
-        let manifests = Manifest::discover(&home, &start_dir)?;
-
-        for manifest in &manifests {
-            if let Some(tool_id) = manifest.tools.get(exe_name.as_str()) {
-                let args = std::env::args().skip(1).collect();
-                std::process::exit(tool_storage.run(tool_id, args).await?);
-            }
-        }
-
-        // If we're in Aftman's bin dir, we know for sure that we were supposed
-        // to be an Aftman tool.
-        let exe_path = current_exe()?;
-        if exe_path.starts_with(&tool_storage.bin_dir) {
-            let manifest_list = manifests
-                .iter()
-                .filter_map(|manifest| {
-                    manifest
-                        .path
-                        .as_ref()
-                        .map(|path| format!("- {}", path.display()))
-                })
-                .collect::<Vec<_>>()
-                .join("\n");
-
-            bail!("Tried to run an Aftman-managed version of {exe_name}, but no aftman.toml files list this tool.\n\
-                To run {exe_name} from this directory, add it to one of these files:\n\
-                {manifest_list}");
-        }
-    }
-
-    Manifest::init_global(&home)?;
-    AuthManifest::init(&home)?;
-    system_path::init(&home)?;
-
-    Args::parse().run(&home, tool_storage).await
-}
-
-fn current_exe_name() -> anyhow::Result<String> {
-    let exe_path = current_exe().context("Failed to discover the name of the Aftman executable")?;
-    let mut exe_name = exe_path
-        .file_name()
-        .and_then(|name| name.to_str())
-        .ok_or_else(|| format_err!("OS gave a funny result when asking for executable name"))?;
-
-    exe_name = exe_name.strip_suffix(EXE_SUFFIX).unwrap_or(exe_name);
-
-    Ok(exe_name.to_owned())
-}
-
-fn main() {
-    use tracing::level_filters::LevelFilter;
-    use tracing_subscriber::EnvFilter;
-
+#[tokio::main]
+async fn main() -> Result<()> {
     let tracing_env_filter = EnvFilter::builder()
         .with_default_directive(LevelFilter::INFO.into())
         .from_env_lossy()
+        // Adding the below extra directives will let us debug
+        // aftman easier using RUST_LOG=debug or RUST_LOG=trace
         .add_directive("reqwest=info".parse().unwrap())
         .add_directive("rustls=info".parse().unwrap())
         .add_directive("hyper=info".parse().unwrap())
         .add_directive("h2=info".parse().unwrap());
+
     tracing_subscriber::fmt()
         .with_env_filter(tracing_env_filter)
         .with_target(false)
         .without_time()
         .init();
 
-    let rt = tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .expect("Failed to create Tokio runtime");
-
-    rt.block_on(async {
-        if let Err(err) = run().await {
-            eprintln!("Aftman error: {:?}", err);
-            std::process::exit(1);
-        }
-    })
+    Args::parse().run().await
 }
