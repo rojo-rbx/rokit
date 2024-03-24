76a72085cc9ced8b16a97eeebca56b3db9e8b47b
Filip Tibell <filip.tibell@gmail.com>
2024-03-24 01:44:55 +0100

    Re-export consistency fix

--- lib/storage/mod.rs
+++ lib/storage/mod.rs
@@ -5,8 +5,8 @@ mod tool_storage;
 mod trust_cache;
 mod util;
 
-pub use home::Home;
-pub use install_cache::InstallCache;
-pub use result::{StorageError, StorageResult};
-pub use tool_storage::ToolStorage;
-pub use trust_cache::TrustCache;
+pub use self::home::Home;
+pub use self::install_cache::InstallCache;
+pub use self::result::{StorageError, StorageResult};
+pub use self::tool_storage::ToolStorage;
+pub use self::trust_cache::TrustCache;
