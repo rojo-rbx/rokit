8fa52d19214d99c6c787acfd472a595a67419b16
Filip Tibell <filip.tibell@gmail.com>
2024-03-24 19:24:30 +0100

    Fix missing re export

--- lib/system/mod.rs
+++ lib/system/mod.rs
@@ -1,5 +1,5 @@
 mod discovery;
 mod runner;
 
-pub use self::discovery::discover_file_recursive;
+pub use self::discovery::{discover_file_recursive, discover_files_recursive};
 pub use self::runner::run_interruptible;
