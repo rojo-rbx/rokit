53163823039bf777dae849515505b78b0bf467c8
Filip Tibell <filip.tibell@gmail.com>
2024-03-26 19:53:40 +0100

    Make the code for it readable

--- src/cli/list.rs
+++ src/cli/list.rs
@@ -13,49 +13,74 @@ pub struct ListSubcommand {
 
 impl ListSubcommand {
     pub async fn run(self, home: &Home) -> Result<()> {
-        let cache = home.tool_cache();
-        let tools = cache
-            .all_installed_ids()
-            .into_iter()
-            .map(|id| (id.clone(), cache.all_installed_versions_for_id(&id)))
-            .collect::<Vec<_>>();
+        let no_tools_installed = home.tool_cache().all_installed_ids().is_empty();
 
-        let header;
-        let mut lines = vec![];
-
-        let list_bullet = style("•").dim();
-        if tools.is_empty() {
-            header = String::from("🛠️  No tools are installed.");
+        let (header, lines) = if no_tools_installed {
+            list_versions_for_empty(home)
         } else if let Some(id) = self.id {
-            let mut versions = cache.all_installed_versions_for_id(&id);
-            versions.reverse(); // List newest versions first
-            if !versions.is_empty() {
-                header = format!("🛠️  Installed versions of {id}:");
-                for version in versions {
-                    lines.push(format!("  {list_bullet} {version}"));
-                }
-            } else {
-                header = format!("🛠️  No versions of {id} are installed.");
-            }
+            list_versions_for_id(home, &id)
         } else {
-            header = String::from("🛠️  All installed tools:");
-            for (id, mut versions) in tools {
-                versions.reverse(); // List newest versions first
-                lines.push(id.to_string());
-                for version in versions {
-                    lines.push(format!("  {list_bullet} {version}"));
-                }
-            }
-        }
+            list_versions_for_all(home)
+        };
 
-        let extra_newline = if !lines.is_empty() { "\n" } else { "" };
-        let message = lines
-            .into_iter()
-            .map(|line| line.to_string())
-            .collect::<Vec<_>>()
-            .join("\n");
-        println!("{header}\n{extra_newline}{message}");
+        println!(
+            "{header}{}{}",
+            if !lines.is_empty() { "\n\n" } else { "\n" },
+            lines.join("\n")
+        );
 
         Ok(())
     }
+}
+
+// No tools are installed - just print out a message
+fn list_versions_for_empty(_home: &Home) -> (String, Vec<String>) {
+    let header = String::from("🛠️  No tools are installed.");
+    (header, Vec::new())
+}
+
+// Lists all versions for a specific tool - if it is installed
+fn list_versions_for_id(home: &Home, id: &ToolId) -> (String, Vec<String>) {
+    let cache = home.tool_cache();
+
+    let mut versions = cache.all_installed_versions_for_id(id);
+    versions.reverse(); // List newest versions first
+
+    if !versions.is_empty() {
+        let header = format!("🛠️  Installed versions of {id}:");
+        let bullet = style("•").dim();
+        let lines = versions
+            .into_iter()
+            .map(|version| format!("  {bullet} {version}"))
+            .collect();
+        (header, lines)
+    } else {
+        let header = format!("🛠️  No versions of {id} are installed.");
+        (header, Vec::new())
+    }
+}
+
+// Lists all versions for all installed tools
+fn list_versions_for_all(home: &Home) -> (String, Vec<String>) {
+    let cache = home.tool_cache();
+    let tools = cache
+        .all_installed_ids()
+        .into_iter()
+        .map(|id| (id.clone(), cache.all_installed_versions_for_id(&id)))
+        .collect::<Vec<_>>();
+
+    let header = String::from("🛠️  All installed tools:");
+    let bullet = style("•").dim();
+    let lines = tools
+        .into_iter()
+        .flat_map(|(id, mut versions)| {
+            versions.reverse(); // List newest versions first
+            let mut lines = vec![id.to_string()];
+            for version in versions {
+                lines.push(format!("  {bullet} {version}"));
+            }
+            lines
+        })
+        .collect();
+    (header, lines)
 }
