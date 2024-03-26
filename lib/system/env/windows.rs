2180680955c404627653ff6c16a95b28994c1b02
Filip Tibell <filip.tibell@gmail.com>
2024-03-26 17:05:12 +0100

    Fix

--- lib/system/env/windows.rs
+++ lib/system/env/windows.rs
@@ -12,7 +12,7 @@ pub async fn add_to_path(home: &Home) -> RokitResult<bool> {
     // NOTE: Calls to canonicalize may use blocking filesystem
     // operations, so we spawn a task where that's acceptable.
     let dir = home.path().join("bin");
-    let task = spawn_blocking(|| {
+    let task = spawn_blocking(move || {
         let dir = dir.canonicalize()?;
 
         let key = RegKey::predef(HKEY_CURRENT_USER);
