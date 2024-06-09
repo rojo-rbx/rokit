0b5b27cd2fbc5de22ce13252a8d824e96b61f4f1
Filip Tibell <filip.tibell@gmail.com>
2024-06-09 19:39:00 +0200

    Implement fall-through mechanism for tools not managed by Rokit

--- src/runner/mod.rs
+++ src/runner/mod.rs
@@ -1,9 +1,9 @@
 use std::{env::args, process::exit, str::FromStr};
 
-use anyhow::{Context, Error, Result};
+use anyhow::{bail, Error, Result};
 
 use rokit::{
-    discovery::discover_tool_spec,
+    discovery::{discover_non_rokit_tool, discover_tool_spec},
     storage::Home,
     system::{current_exe_name, run_interruptible},
     tool::ToolAlias,
@@ -32,21 +32,24 @@ impl Runner {
         let alias = ToolAlias::from_str(&self.exe_name)?;
 
         let home = Home::load_from_env().await?;
-        let spec = discover_tool_spec(&alias, false, false)
-            .await
-            .with_context(|| {
-                format!(
+        let spec = discover_tool_spec(&alias, false, false).await;
+
+        let program_args = args().skip(1).collect::<Vec<_>>();
+        let program_path = match spec {
+            // TODO: Prompt for trust and install tool if not already installed
+            Some(spec) => home.tool_storage().tool_path(&spec),
+            // FUTURE: Maybe we should add some kind of "fall-through" setting in
+            // Rokit manifests instead of always falling through to non-rokit tools?
+            None => match discover_non_rokit_tool(&home, &alias).await {
+                Some(path) => path,
+                None => bail!(
                     "Failed to find tool '{alias}' in any project manifest file.\
                     \nAdd the tool to a project using 'rokit add' before running it."
-                )
-            })?;
+                ),
+            },
+        };
 
-        // TODO: Prompt for trust and install tool if not already installed
-
-        let path = home.tool_storage().tool_path(&spec);
-        let args = args().skip(1).collect::<Vec<_>>();
-
-        let code = run_interruptible(&path, &args)
+        let code = run_interruptible(&program_path, &program_args)
             .await
             .map_err(Error::from)
             .inspect_err(|e| inform_user_about_potential_fixes(&alias, e))?;
