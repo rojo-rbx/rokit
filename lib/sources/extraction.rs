cb777a306f766bdb78c6b356c4335a268518c835
Filip Tibell <filip.tibell@gmail.com>
2024-06-09 18:02:53 +0200

    Update dependencies

--- lib/sources/extraction.rs
+++ lib/sources/extraction.rs
@@ -9,7 +9,7 @@ use std::{
 use tar::Archive as TarArchive;
 use thiserror::Error;
 use tokio::{task::spawn_blocking, time::Instant};
-use zip_next::ZipArchive;
+use zip::ZipArchive;
 
 use crate::{descriptor::OS, result::RokitResult, sources::ArtifactFormat};
 
