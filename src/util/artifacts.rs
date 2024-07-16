4a9fff37109a5dbd948587d9da9b3048111c08f1
Filip Tibell <filip.tibell@gmail.com>
2024-07-16 13:44:01 +0200

    Remove user-facing warnings in favor of better tracing when looking for artifact fallbacks

--- src/util/artifacts.rs
+++ src/util/artifacts.rs
@@ -1,8 +1,8 @@
 use anyhow::{Context, Result};
 
 use rokit::{
-    descriptor::Descriptor,
-    sources::{Artifact, ArtifactProvider},
+    descriptor::{Arch, OS},
+    sources::Artifact,
     tool::ToolId,
 };
 
@@ -12,58 +12,27 @@ pub fn find_most_compatible_artifact(artifacts: &[Artifact], tool_id: &ToolId) -
         .cloned();
 
     if artifact_opt.is_none() {
-        let current_desc = Descriptor::current_system();
-
-        // If we failed to find an artifact compatible with the current system,
-        // we may be able to give additional information to Rokit's users, or tool
-        // maintainers who want to be Rokit-compatible, by examining the artifacts
-        let no_artifacts_with_arch = artifacts.iter().all(|artifact| {
-            artifact
-                .name
-                .as_deref()
-                .and_then(Descriptor::detect)
-                .map_or(false, |desc| desc.arch().is_none())
-        });
-        let additional_information = if no_artifacts_with_arch {
-            let source_is_github = artifacts
-                .iter()
-                .all(|artifact| matches!(artifact.provider, ArtifactProvider::GitHub));
-            let source_name = if source_is_github {
-                "GitHub release files"
-            } else {
-                "tool release files"
-            };
-            Some(format!(
-                "This seems to have been caused by {0} not \
-                specifying an architecture in any of its artifacts.\
-                \nIf you are the maintainer of this tool, you can resolve \
-                this issue by specifying an architecture in {source_name}:\
-                \n    {0}-{1}-{2}.zip",
-                tool_id.name(),
-                current_desc.os().as_str(),
-                current_desc.arch().expect("no current arch (??)").as_str(),
-            ))
-        } else {
-            None
-        };
-
-        // Let the user know about failing to find an artifact,
-        // potentially with additional information generated above
-        tracing::warn!(
-            "Failed to find a fully compatible artifact for {tool_id}!{}\
-            \nSearching for a fallback...",
-            match additional_information {
-                Some(info) => format!("\n{info}"),
-                None => String::new(),
-            }
-        );
-
         if let Some(artifact) = Artifact::find_partially_compatible_fallback(artifacts) {
-            tracing::info!(
-                "Found fallback artifact '{}' for tool {tool_id}",
-                artifact.name.as_deref().unwrap_or("N/A")
+            tracing::debug!(
+                %tool_id,
+                name = %artifact.name.as_deref().unwrap_or("N/A"),
+                "found fallback artifact for tool",
             );
             artifact_opt.replace(artifact);
+        } else {
+            // If we failed to find an artifact compatible with the current system,
+            // we may be able to give additional information to Rokit's users, or tool
+            // maintainers who want to be Rokit-compatible, by examining the artifacts
+            let artifact_names = artifacts
+                .iter()
+                .filter_map(|artifact| artifact.name.as_deref())
+                .collect::<Vec<_>>();
+            tracing::debug!(
+                %tool_id,
+                missing_os_all = %artifact_names.iter().all(|s| OS::detect(s).is_none()),
+                missing_arch_all = %artifact_names.iter().all(|s| Arch::detect(s).is_none()),
+                "missing compatible artifact or fallback for tool"
+            );
         }
     }
 
